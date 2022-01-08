7ca0a077d4d38debe3f290ccbe3a012a2db74e3f
rjp311 <43649055+rjp311@users.noreply.github.com>
2022-01-08 18:46:59 +0000

    Initial test commit

---
+++ pages/index.js
@@ -0,0 +1,21 @@
+import Head from "next/head";
+import { Component } from "react";
+import { attributes, react as HomeContent} from "../content/home.md";
+
+export default class Home extends Component {
+  redner() {
+    let {title} = attributes;
+    return (
+      <>
+        <Head>
+          <title>Home | Grantham Swimming Club</title>
+          <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
+        </Head>
+        <article>
+          <h1>{title}</h1>
+          <HomeContent />
+        </article>
+      </>
+    )
+  }
+}
